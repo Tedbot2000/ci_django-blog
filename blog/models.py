0c4719d8c5bc28eccb168dd36ff0b1924a89524b
Edward Aylmer <mail@tedaylmer.co.uk>
2024-07-26 11:01:59 +0000

    add edit and delete functions

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
+
+# Create your models here.
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -13,8 +15,9 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    STATUS = ((0, "Draft"), (1, "Published"))
-    
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
     class Meta:
         ordering = ["-created_on"]
 
@@ -23,23 +26,16 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name="comments"
-    )
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="comments_author"
-    )
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
-    approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
 
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-
